4e195d5c04641d808c9416865ada7e102163c5c5
Eric-is-good <73528862+Eric-is-good@users.noreply.github.com>
2021-11-18 17:11:40 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+﻿#include "AvlTree.hpp"
+#include "show_tree.h"
+
+
+int main()
+{
+    AvlTree<int> tree;
+    //BTree t_to_show;  //show
+    
+    int value;
+    int tmp;
+    cout << "请输入整数建立二叉树(-1结束)：" << endl;
+    while (cin >> value)
+    {
+        if (value == -1)
+            break;
+        tree.Insert(tree.root, value);
+    }
+    show_structure(tree);
+
+    while (1) {
+        cout << "请输入要删除的结点：";
+        cin >> tmp;
+        tree.Delete(tree.root, tmp);
+        show_structure(tree);
+
+        cout << "请输入要插入的结点：";
+        cin >> tmp;
+        tree.Insert(tree.root, tmp);
+        show_structure(tree);
+    }
+    
+
+}
